1394ce9d6797db46836f42dd1fa6c74615476e4b
amirazzam2000 <ameerazzam123@gmail.com>
2021-04-08 07:47:39 +0200

    initial setup

---
+++ src/classes/FileReader/FileReader.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+using namespace std;
+
+class FileReader {
+    private:
+        string path;
+
+
+    public:
+        FileReader(string path){
+            this->path = path;
+        }
+
+}
